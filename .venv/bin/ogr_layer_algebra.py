1d961ea68440e93d830ea64041304f8337e0559f
theo24-sys <timothybanjo42@gmail.com>
2026-06-07 13:18:16 +0300

    Initial commit

---
+++ .venv/bin/ogr_layer_algebra.py
@@ -0,0 +1,12 @@
+#!/home/banjo-9/Desktop/BodaLance/salamanoda/.venv/bin/python3
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.ogr_layer_algebra as a convenience to use as a script
+from osgeo_utils.ogr_layer_algebra import *  # noqa
+from osgeo_utils.ogr_layer_algebra import main
+
+deprecation_warn("ogr_layer_algebra")
+sys.exit(main(sys.argv))
---
+++ .venv/bin/pct2rgb.py
@@ -0,0 +1,12 @@
+#!/home/banjo-9/Desktop/BodaLance/salamanoda/.venv/bin/python3
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.pct2rgb as a convenience to use as a script
+from osgeo_utils.pct2rgb import *  # noqa
+from osgeo_utils.pct2rgb import main
+
+deprecation_warn("pct2rgb")
+sys.exit(main(sys.argv))
